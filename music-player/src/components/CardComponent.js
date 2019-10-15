4f6c740078e997c257222f4a85322095da32b040
Aman Kumar Tiwari <kumaramantiwari98@gmail.com>
2019-10-15 20:14:29 +0530

    style added

--- music-player/src/components/CardComponent.js
+++ music-player/src/components/CardComponent.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import '../App.css'
 import coldplay from '../images/coldplay.jpg'
 import panda from '../images/panda.jpg'
 import imagine from '../images/imagine.jpg'
@@ -7,9 +8,11 @@ class CardComponent extends React.Component{
     render(){
         return(
         <React.Fragment>
-              <img src={coldplay} alt="Logo" height="200" width="300" />
-              <img src={panda} alt="Logo" height="200" width="300" style={{margin : '0 35px'}} />
-              <img src={imagine} alt="Logo" height="200" width="300" />             
+             <div className="imgHover" > 
+                    <img src={coldplay} alt="Logo" height="200" width="300" />
+                    <img src={panda} alt="Logo" height="200" width="300" style={{margin : '0 35px'}} />
+                    <img src={imagine} alt="Logo" height="200" width="300" />             
+             </div>
         </React.Fragment>
         );
     }
