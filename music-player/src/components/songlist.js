32f0869f582db50d28f4ddd4e93d3384a5307771
Aman Kumar Tiwari <kumaramantiwari98@gmail.com>
2019-10-31 02:01:37 +0530

    added lists

--- music-player/src/components/songlist.js
+++ music-player/src/components/songlist.js
@@ -6,32 +6,52 @@ const {Paragraph} = Typography
 let myobj = [
    {
        id:123,
-       name: 'ABCD',
+       SongName: 'ABCD',
+       Artist: 'Joseph Stalin',
+       Album: "I'm The One",
        preview:"https://cdns-preview-0.dzcdn.net/stream/c-096452f45cdca8793b4f01d33189c559-6.mp3",
        img:'asd',
-   }
-
+   },{
+    id:13,
+    SongName: 'ABCD',
+    Artist: 'Vladimir Putin',
+    Album: "I'm The One",
+    preview:"https://cdns-preview-0.dzcdn.net/stream/c-096452f45cdca8793b4f01d33189c559-6.mp3",
+    img:'asd',
+},{
+    id:23,
+    SongName: 'ABCD',
+    Artist: 'Donald Trump',
+    Album: "I'm The One",
+    preview:"https://cdns-preview-0.dzcdn.net/stream/c-096452f45cdca8793b4f01d33189c559-6.mp3",
+    img:'asd',
+}
 ]
 
 class Songlist extends React.Component{
 
   render(){
       return(
-         <Row type="flex" justify="middle" style={{backgroundColor: 'rgb(13, 13, 255)',height:'90px',}}>
-              <Col span={4}>
-                  <img  src={coldplay} alt="aman" height="70px" width="70px" style={{marginLeft:'30px'}} ></img>
-              </Col>
-                
-                <Col span={4}  >
-                      <Paragraph style={{fontSize:'18px',marginTop:'8px',color:'white',fontWeight:'bold' } } > Aman Kumar </Paragraph>
-                      <Paragraph style={{fontSize:'14px',marginTop:'8px',color:'white' }} > Rise above hate </Paragraph>
-                </Col>
+        
+     myobj.map( (val,id) => {
+                return (
+          <Row key={id} type="flex" justify="middle" style={{backgroundColor: 'rgb(13, 13, 255)',height:'90px',margin:'15px 0'}}>
+                  <Col span={4}>
+                    <img  src={coldplay} alt="aman" height="70px" width="70px" style={{marginLeft:'30px'}} ></img>
+                  </Col>
+                  
+                  <Col span={4}  >
+                        <Paragraph style={{fontSize:'18px',marginTop:'8px',color:'white',fontWeight:'bold' } } > {val.SongName} </Paragraph>
+                        <Paragraph style={{fontSize:'14px',marginTop:'8px',color:'white' }} > {val.Artist} </Paragraph>
+                  </Col>
+  
+                  <Col>
+                      <Icon type="heart" theme="twoTone"  width="40px" height="40px" />
+                  </Col>
+  
+           </Row>)
+            })
 
-                <Col>
-                    <Icon type="heart" theme="twoTone"  width="40px" height="40px" />
-                </Col>
-
-         </Row>
       );
   }
 
